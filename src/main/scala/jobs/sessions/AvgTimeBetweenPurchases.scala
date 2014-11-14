bd9284d8ba1802df1605bf3f017f07dec2e28285
Joao Vasques <joao.vasques@tecnico.ulisboa.pt>
2014-11-14 23:15:23 +0000

    implements avg time between purchases

--- src/main/scala/jobs/sessions/AvgTimeBetweenPurchases.scala
+++ src/main/scala/jobs/sessions/AvgTimeBetweenPurchases.scala
@@ -1,3 +1,142 @@
 package wazza.thor.jobs
 
-//avg time bet purchases
+import com.typesafe.config.{Config, ConfigFactory}
+import java.text.SimpleDateFormat
+import java.util.Date
+import org.apache.spark._
+import scala.util.Try
+import org.apache.hadoop.conf.Configuration
+import org.bson.BSONObject
+import org.bson.BasicBSONObject
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.hadoop.conf.Configuration
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import scala.collection.immutable.StringOps
+import wazza.thor.messages._
+import com.mongodb.casbah.Imports._
+import play.api.libs.json._
+import org.joda.time.LocalDate
+
+object AvgTimeBetweenPurchases {
+  def props(sc: SparkContext): Props = Props(new AvgTimeBetweenPurchases(sc))
+}
+
+class AvgTimeBetweenPurchases(sc: SparkContext) extends Actor with ActorLogging  with ChildJob {
+  import context._
+
+  def inputCollectionType: String = "purchases"
+  def outputCollectionType: String = "AvgTimeBetweenPurchases"
+
+  private def saveResultToDatabase(
+    uriStr: String,
+    collectionName: String,
+    result: Double,
+    end: Date,
+    start: Date,
+    companyName: String,
+    applicationName: String
+  ) = {
+    val uri  = MongoClientURI(uriStr)
+    val client = MongoClient(uri)
+    val collection = client.getDB(uri.database.get)(collectionName)
+    val obj = MongoDBObject(
+      "avgTimeBetweenPurchases" -> result,
+      "lowerDate" -> start.getTime,
+      "upperDate" -> end.getTime
+    )
+    collection.insert(obj)
+    client.close
+  }
+
+  def executeJob(
+    companyName: String,
+    applicationName: String,
+    start: Date,
+    end: Date
+  ): Future[Unit] = {
+    val promise = Promise[Unit]
+
+    def getNumberSecondsBetweenDates(d1: Date, d2: Date): Float = {
+      (new LocalDate(d2).toDateTimeAtCurrentTime.getMillis - new LocalDate(d1).toDateTimeAtCurrentTime().getMillis) / 1000
+    }
+
+    val dateFields = ("lowerDate", "upperDate")
+    val payingUsersCollName = s"${companyName}_payingUsers_${applicationName}"
+    val uri = MongoClientURI(ThorContext.URI)
+    val mongoClient = MongoClient(uri)
+    val payingUsersCollection = mongoClient(uri.database.getOrElse("dev"))(payingUsersCollName)
+
+    val query = (dateFields._1 $gte end.getTime $lte start.getTime) ++ (dateFields._2 $gte end.getTime $lte start.getTime)
+    val payingUsersOpt = payingUsersCollection.findOne(query).getOrElse(None)
+
+    payingUsersOpt match {
+      case None => {
+        log.error("cannot find elements")
+        promise.failure(new Exception)
+      }
+      case payingUsers => {
+        var numberPurchases = 0
+        var accTime = 0.0
+        for {
+          userInfo <- (Json.parse(payingUsers.toString) \ "payingUsers").as[JsArray].value
+        } {
+          val purchases = (userInfo \ "purchases").as[JsArray].value.zipWithIndex
+          for(purchaseInfo <- purchases) {
+            val index = purchaseInfo._2
+            if(purchases.size == 1) {
+              numberPurchases += 1
+            } else {
+              if((index+1) < purchases.size ) {
+                val currentPurchaseDate = new Date((purchaseInfo._1 \ "purchaseTime").as[Float].longValue)
+                val nextPurchaseDate = new Date((purchases(index+1)._1 \ "purchaseTime").as[Float].longValue)
+                accTime += getNumberSecondsBetweenDates(currentPurchaseDate, nextPurchaseDate)
+              }
+            }
+          }
+        }
+
+        val result =  if(numberPurchases == 0) 0 else accTime / numberPurchases
+        saveResultToDatabase(
+          ThorContext.URI,
+          getCollectionOutput(companyName, applicationName),
+          result,
+          start,
+          end,
+          companyName,
+          applicationName
+        )
+        promise.success()
+      }
+    }
+
+    mongoClient.close
+    promise.future
+  }
+
+  def kill = stop(self)
+
+  def receive = {
+    case CoreJobCompleted(companyName, applicationName, name, lower, upper) => {
+      log.info(s"core job ended ${sender.toString}")
+      updateCompletedDependencies(sender)
+      if(dependenciesCompleted) {
+        log.info("execute job")
+        executeJob(companyName, applicationName, upper, lower) map { arpu =>
+          log.info("Job completed successful")
+          onJobSuccess(companyName, applicationName, "Average Purchases Per Session")
+        } recover {
+          case ex: Exception => onJobFailure(ex, "Average Purchases Per Session")
+        }
+      }
+    }
+    case CoreJobDependency(ref) => {
+      log.info(s"Updating core dependencies: ${ref.toString}")
+      addDependencies(ref)
+    }
+    case _ => log.debug("Received invalid message")
+  }
+}
+
